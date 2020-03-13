1d5f5c3ffa4956e51b92620867ac06e3e4bbb2b9
lethanhhai1998 <49983494+lethanhhai1998@users.noreply.github.com>
2020-03-13 17:30:57 +0700

    add all

--- src/main/java/com/heathens/music/MusicApplication.java
+++ src/main/java/com/heathens/music/MusicApplication.java
@@ -2,8 +2,14 @@ package com.heathens.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan({"com.heathens.music.controller","com.heathens.music.service"})
+@EntityScan("com.heathens.music.model")
 public class MusicApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/heathens/music/controller/SongController.java
+++ src/main/java/com/heathens/music/controller/SongController.java
@@ -1,13 +1,28 @@
 package com.heathens.music.controller;
 
-
+import com.heathens.music.model.Song;
+import com.heathens.music.service.ISongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @CrossOrigin("*")
-@RequestMapping("/api/song")
+@RequestMapping(value = "/api/songs")
 public class SongController {
+    @Autowired
+    private ISongService songService;
 
-}
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<Song>> getAllSong() {
+        List<Song> songs = this.songService.findAll();
+        if(songs == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+ }
